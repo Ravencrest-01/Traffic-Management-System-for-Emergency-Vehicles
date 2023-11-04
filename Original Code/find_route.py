ab7569e6ea9ea0b983ca327a7cf7d15700d67432
Ritik Kumar (Ravencrest) <ritiksingh1859@gmail.com>
2023-11-05 01:56:36 +0530

    Updates

--- Original Code/find_route.py
+++ Original Code/find_route.py
@@ -1,26 +1,36 @@
 from collections import deque
-
-def print_arrows(path, movements):
-    arrow_symbols = {'^': (1, 0), 'v': (-1, 0), '>': (0, 1), '<': (0, -1)}
-    for i in range(1, len(path)):
-        row, col = path[i]
-        prev_row, prev_col = path[i - 1]
-        direction = None
-        for dir, (dr, dc) in arrow_symbols.items():
-            if (row - prev_row == dr and col - prev_col == dc):
-                direction = dir
-                break
-        print(direction, end=' ')
+import sys
 
 def find_route(current_location, final_location):
-    movements = [(0, 1, '>'), (0, -1, '<'), (1, 0, 'v'), (-1, 0, '^')]
+    """Finds the route from the current location to the final location.
 
+    Args:
+        current_location: A tuple of (row, col) representing the current location.
+        final_location: A tuple of (row, col) representing the final location.
+
+    Returns:
+        A list of directions, where each direction is a string representing one of
+        the following characters: '^', 'v', '<', or '>'.
+    """
+
+    movements = [(0, 1, '>'), (0, -1, '<'), (1, 0, 'v'), (-1, 0, '^')]
+    # 7 x 7 City Grid
     city_grid = [
-        [0, 0, 0, 0, 0],
-        [0, 1, 1, 0, 0],
-        [0, 0, 0, 0, 1],
-        [0, 0, 1, 1, 1],
-        [0, 0, 0, 0, 0]
+        # [0, 1, 0, 0, 0, 0, 0],
+        # [0, 1, 0, 0, 0, 0, 0],
+        # [0, 1, 0, 1, 0, 0, 0],
+        # [0, 0, 1, 0, 0, 0, 0],
+        # [0, 0, 0, 0, 1, 0, 0],
+        # [0, 0, 0, 0, 1, 0, 0],
+        # [0, 0, 0, 0, 0, 0, 0]
+
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0]
     ]
 
     rows, cols = len(city_grid), len(city_grid[0])
@@ -31,9 +41,9 @@ def find_route(current_location, final_location):
     final_row, final_col = final_location
 
     if (0 <= current_row < rows and 0 <= current_col < cols and
-            0 <= final_row < rows and 0 <= final_col < cols and
-            city_grid[current_row][current_col] == 0 and
-            city_grid[final_row][final_col] == 0):
+        0 <= final_row < rows and 0 <= final_col < cols and
+        city_grid[current_row][current_col] == 0 and
+        city_grid[final_row][final_col] == 0):
 
         queue = deque([(current_row, current_col)])
         visited[current_row][current_col] = True
@@ -48,8 +58,8 @@ def find_route(current_location, final_location):
                 new_row, new_col = row + dr, col + dc
 
                 if (0 <= new_row < rows and 0 <= new_col < cols and
-                        city_grid[new_row][new_col] == 0 and
-                        not visited[new_row][new_col]):
+                    city_grid[new_row][new_col] == 0 and
+                    not visited[new_row][new_col]):
                     queue.append((new_row, new_col))
                     visited[new_row][new_col] = True
                     parent[(new_row, new_col)] = (row, col)
@@ -62,16 +72,31 @@ def find_route(current_location, final_location):
         path.append((current_row, current_col))
         path.reverse()
 
-        print("Starting from", (current_row, current_col))
-        print("Going to", (final_location))
-        print("Directions:")
-        print_arrows(path, movements)
-        print("\nDestination reached")
+        directions = []
+        for i in range(1, len(path)):
+            row, col = path[i]
+            prev_row, prev_col = path[i - 1]
+
+            direction = None
+            for dr, dc, dir in movements:
+                if (row - prev_row == dr and col - prev_col == dc):
+                    direction = dir
+                    break
+
+            directions.append(direction)
+
+        return directions
 
     else:
-        print("Invalid current or final location.")
+        return None
 
-# Example usage:
-current_location = (0, 0)
-final_location = (4, 4)
-find_route(current_location, final_location)
+if __name__ == '__main__':
+    current_location = tuple(map(int, sys.argv[1].split(',')) if len(sys.argv) > 1 else (1, 1))
+    final_location = tuple(map(int, sys.argv[2].split(',')) if len(sys.argv) > 2 else (6, 6))
+
+    directions = find_route(current_location, final_location)
+
+    if directions:
+        print("".join(directions))
+    else:
+        print("None")
